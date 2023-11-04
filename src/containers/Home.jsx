7d4c3745d54f538408964c15fea769641e255b76
DoyleJ11 <jackconnerdoyle@gmail.com>
2023-11-03 22:17:53 -0700

    Fixed mobile scrolling (Hopefully)

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -9,15 +9,19 @@ import CustomCursor from "../components/CustomCursor";
 export default function Home() {
   const menuItems = useRef(null);
   const [renderItems, setRenderItems] = useState(pageData);
+  const [clonesHeight, setClonesHeight] = useState(0);
 
   const cloneItems = () => {
     const itemHeight = menuItems.current.childNodes[0].offsetHeight;
-    const fitMax = Math.ceil(window.innerHeight / itemHeight);
+    const screenHeight = window.innerHeight;
+    const fitMax = Math.ceil(screenHeight / itemHeight);
 
     const clonedItems = renderItems.slice(0, fitMax);
 
     setRenderItems([...renderItems, ...clonedItems]);
-    return clonedItems.length * itemHeight;
+    const newClonesHeight = clonedItems.length * itemHeight;
+    setClonesHeight(newClonesHeight);
+    return newClonesHeight;
   };
 
   const getScrollPos = () => {
@@ -29,28 +33,32 @@ export default function Home() {
   };
 
   useEffect(() => {
-    const clonesHeight = cloneItems();
+    cloneItems();
 
     menuItems.current.style.scrollBehavior = "unset";
     const scrollUpdate = () => {
       const scrollPos = getScrollPos();
+
       if (scrollPos <= 0) {
-        setScrollPos(clonesHeight);
-      } else if (scrollPos + menuItems.current.clientHeight >= menuItems.current.scrollHeight) {
+        // Set the scroll position to the bottom of the cloned items with a buffer
+        setScrollPos(clonesHeight - 1);
+      } else if (scrollPos + menuItems.current.clientHeight >= menuItems.current.scrollHeight - 1) {
+        // Buffer to ensure we're not resetting during momentum scroll
         setScrollPos(1);
       }
     };
-    
+
+    // Consider debouncing this if it fires too often
     menuItems.current.addEventListener("scroll", scrollUpdate);
 
     return () => {
       menuItems.current.removeEventListener("scroll", scrollUpdate);
     };
-  }, []);
+  }, [clonesHeight]);
 
   return (
     <CursorManager>
-        <CustomCursor />
+      <CustomCursor />
       <Header />
       <div className="main-container" id="main-container">
         <ul ref={menuItems}>
